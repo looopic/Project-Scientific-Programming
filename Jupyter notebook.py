49b94b8ed76ba3aff5ba577848b2c6a39271ec44
leurom <95762671+leurom@users.noreply.github.com>
2022-05-18 15:59:23 +0200

    Add files via upload

---
+++ Jupyter notebook.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[9]:
+
+
+data = pd.read_csv(r'C:\Users\leuro\OneDrive\ZHAW\Wirtschaftsinformatik\14_Scientific Programming\imdb_top_1000.csv', sep=",")
+print(data.head(5))
+
+
+# In[ ]:
+
+
+
+
